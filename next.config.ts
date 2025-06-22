36b98893e60e6c91a3fbb0cc3aaa8a869d620d26
hannah0wang <hannah0ananas@gmail.com>
2025-06-22 18:40:53 -0400

    update middleware.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    nodeMiddleware: true, // TEMPORARY: Only needed until Edge runtime support is added
+  },
 };
 
 export default nextConfig;
